d0a3cc8f0ffaee82d64977a16b469beefef2134a
Deepak0Prajapati <deepakprajapatibluethink@gmail.com>
2026-03-12 19:10:39 +0530

    api gateway addition

--- activityservice/src/main/java/com/fitness/activityservice/config/WebClientConfig.java
+++ activityservice/src/main/java/com/fitness/activityservice/config/WebClientConfig.java
@@ -16,7 +16,7 @@ public class WebClientConfig {
 
     @Bean
     public WebClient userServiceWebClient(WebClient.Builder builder){
-        return builder.baseUrl("http://USERSERVICE")
+        return builder.baseUrl("http://USER-SERVICE")
                 .build();
     }
 
